088035682432c0fee0db45073504dedd84a138d7
EnwinDang <n9uyend4ng@gmail.com>
2026-03-13 15:32:04 +0100

    update config.py

--- global_localisation/config.py
+++ global_localisation/config.py
@@ -7,3 +7,8 @@ CALIBRATION_IDS = [0, 1, 2, 3, 4, 5]
 
 # Camera index (start with one)
 CAMERA_INDEX = 2
+
+# MQTT broker
+MQTT_BROKER = "localhost"
+MQTT_PORT = 1883
+MQTT_TOPIC_PREFIX = "city/robots/tag"
